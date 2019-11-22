20104cdfc64bb385f1ed862e454fb5c6fb152c20
sachinsuresh <sachinsuresh00@gmail.com>
2019-11-22 12:09:36 +0530

    User authentication tentatively done

---
+++ UrbanClapProject/accounts/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register', views.register, name='register'),
+    path('users/', views.UserView),
+]
